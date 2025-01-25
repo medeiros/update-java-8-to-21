8dcb31a59ffed1b54c7e921c32fb723b181f5b4d
medeiros <daniel@arneam.com>
2025-01-24 22:16:20 -0300

    Add more Supplier examples

--- src/main/java/com/arneam/java8/lambda/SupplierLambda.java
+++ src/main/java/com/arneam/java8/lambda/SupplierLambda.java
@@ -1,11 +1,22 @@
 package com.arneam.java8.lambda;
 
+import java.time.LocalTime;
 import java.util.function.Supplier;
 
 public class SupplierLambda {
 
-    public String supply() {
+    public String supplyMovie() {
         Supplier<StringBuilder> s = () -> new StringBuilder("ironMan");
         return s.get().toString();
+    }
+
+    public double supplyRandom() {
+        Supplier<Double> s = Math::random;
+        return s.get();
+    }
+
+    public LocalTime supplyTime() {
+        Supplier<LocalTime> lt = LocalTime::now;
+        return lt.get();
     }
 }
--- src/test/java/com/arneam/java8/lambda/SupplierLambdaTest.java
+++ src/test/java/com/arneam/java8/lambda/SupplierLambdaTest.java
@@ -2,9 +2,10 @@ package com.arneam.java8.lambda;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalTime;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 
 class SupplierLambdaTest {
@@ -12,7 +13,9 @@ class SupplierLambdaTest {
     @Test
     void shouldSupplyStringBuilder() {
         SupplierLambda supplierLambda = new SupplierLambda();
-        assertThat(supplierLambda.supply(), is(equalTo("ironMan")));
+        assertThat(supplierLambda.supplyMovie(), is(equalTo("ironMan")));
+        assertThat(supplierLambda.supplyRandom(), is(closeTo(1, 1)));
+        assertThat(supplierLambda.supplyTime(), is(notNullValue(LocalTime.class)));
     }
 
 }
