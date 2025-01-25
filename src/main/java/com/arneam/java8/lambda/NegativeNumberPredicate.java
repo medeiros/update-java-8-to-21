6237ee2d4742f5c28454b432dfcfcc456bb9fa1f
medeiros <daniel@arneam.com>
2025-01-24 21:32:01 -0300

    Add predicate explanation comment

--- src/main/java/com/arneam/java8/lambda/NegativeNumberPredicate.java
+++ src/main/java/com/arneam/java8/lambda/NegativeNumberPredicate.java
@@ -2,6 +2,8 @@ package com.arneam.java8.lambda;
 
 import java.util.function.Predicate;
 
+// Predicate<Integer> is a functional interface (one abstract method)
+//      boolean test(T t)
 public class NegativeNumberPredicate implements Predicate<Integer> {
     @Override
     public boolean test(Integer integer) {
