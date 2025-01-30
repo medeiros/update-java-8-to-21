d9fda3f35b1aa0e68dd9b8352dd6a405635fcdd3
Daniel Assis <daniel@arneam.com>
2025-01-30 10:15:14 -0300

    Add JUnit mechanism to parameterized tests

--- src/test/java/com/arneam/java8/lambda/FunctionLambdaTest.java
+++ src/test/java/com/arneam/java8/lambda/FunctionLambdaTest.java
@@ -1,6 +1,8 @@
 package com.arneam.java8.lambda;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -8,11 +10,12 @@ import static org.hamcrest.Matchers.is;
 
 class FunctionLambdaTest {
 
-    @Test
-    void shouldGetStringSize() {
+    @ParameterizedTest
+    @CsvSource({"daniel,6", "maria,5", "diana,5", "patricia,8"})
+    void shouldGetStringSize(String name, int size) {
         FunctionLambda functionLambda = new FunctionLambda();
-        assertThat(functionLambda.stringSize("daniel"),
-                is(equalTo(6)) );
+        assertThat(functionLambda.stringSize(name),
+                is(equalTo(size)));
     }
 
 }
