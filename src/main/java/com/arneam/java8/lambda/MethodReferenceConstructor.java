c24add19f549cc1c6e67f3f998b8586aff8dd9de
medeiros <daniel@arneam.com>
2025-02-02 18:01:31 -0300

    Add constructor method reference

---
+++ src/main/java/com/arneam/java8/lambda/MethodReferenceConstructor.java
@@ -0,0 +1,33 @@
+package com.arneam.java8.lambda;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class MethodReferenceConstructor {
+    public String concat(String name1, String name2) {
+        return concatWithNoArgsConstructor(name1, name2);
+    }
+
+    public String concatUpper(String name1, String name2) {
+        return concatWithSingleArgConstructor(name1, name2);
+    }
+
+    private static String concatWithNoArgsConstructor(String name1, String name2) {
+        // method reference is related to object creation with ::new (when the only
+        //      thing that the method do is to create an instance
+        // can be called as lambda: "() -> new StringBuilder()"
+        Supplier<StringBuilder> s = StringBuilder::new;
+        return s.get().append(name1).append(" and ").append(name2).toString();
+    }
+
+    private static String concatWithSingleArgConstructor(String name1, String name2) {
+        // method reference is related to object creation with ::new (when the only
+        //      thing that the method do is to create an instance
+        // It can take one parameter and still uses the same MR syntax with ::new
+        //      (the parameter is inferred)
+        // can be called as lambda: "(s) -> new StringBuilder(s)"
+        Function<String, StringBuilder> f = StringBuilder::new;
+        return f.apply(name1.toUpperCase()).append(" AND ")
+                .append(name2.toUpperCase()).toString();
+    }
+}
---
+++ src/test/java/com/arneam/java8/lambda/MethodReferenceConstructorTest.java
@@ -0,0 +1,26 @@
+package com.arneam.java8.lambda;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+
+class MethodReferenceConstructorTest {
+
+    @Test
+    void shouldCreateConstructorMethodReference() {
+        MethodReferenceConstructor m = new MethodReferenceConstructor();
+        assertThat(m.concat("john", "mary"), is(equalTo("john and mary")));
+    }
+
+    @Test
+    void shouldCreateConstructorMethodReferenceWithUppercase() {
+        MethodReferenceConstructor m = new MethodReferenceConstructor();
+        assertThat(m.concatUpper("john", "mary"), is(equalTo("JOHN AND MARY")));
+    }
+
+}
