22b8b200afef782405755086f4d75143dcdb92bc
medeiros <daniel@arneam.com>
2025-02-02 17:09:50 -0300

    Add bounded method reference example

---
+++ src/main/java/com/arneam/java8/lambda/MethodReferenceBounded.java
@@ -0,0 +1,14 @@
+package com.arneam.java8.lambda;
+
+import java.util.function.Predicate;
+
+public class MethodReferenceBounded {
+    String name = "Mr. Crowley";
+    public boolean test(String s) {
+        // method reference is bounded to the variable
+        // it can also be written as lambda: "s -> name.startsWith(s)"
+        // each method reference is converted to lambda by the compiler
+        Predicate<String> predicatePronoun = name::startsWith;
+        return predicatePronoun.test(s);
+    }
+}
---
+++ src/test/java/com/arneam/java8/lambda/MethodReferenceBoundedTest.java
@@ -0,0 +1,17 @@
+package com.arneam.java8.lambda;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class MethodReferenceBoundedTest {
+
+    @Test
+    void shouldExecuteBoundedMethodReference() {
+        MethodReferenceBounded m = new MethodReferenceBounded();
+        assertThat(m.test("Mr. "), is(true));
+        assertThat(m.test("Ms. "), is(false));
+    }
+
+}
