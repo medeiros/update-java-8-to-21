1e356785bb318f658cd818d16ba2af107a83a400
medeiros <daniel@arneam.com>
2025-02-02 17:30:43 -0300

    Add bounded method reference

---
+++ src/main/java/com/arneam/java8/lambda/MethodReferenceUnbounded.java
@@ -0,0 +1,16 @@
+package com.arneam.java8.lambda;
+
+
+import java.util.function.Function;
+
+public class MethodReferenceUnbounded {
+
+    public String apply(String name) {
+        // method reference is unbounded, because there is no variable
+        // or attribute to reference
+        // it can also be written as lambda: "s -> s.toUpperCase()"
+        // each method reference is converted to lambda by the compiler
+        Function<String, String> f = String::toUpperCase;
+        return f.apply(name);
+    }
+}
---
+++ src/test/java/com/arneam/java8/lambda/MethodReferenceUnboundedTest.java
@@ -0,0 +1,20 @@
+package com.arneam.java8.lambda;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+class MethodReferenceUnboundedTest {
+
+    @ParameterizedTest
+    @CsvSource({"john,JOHN","mary,MARY"})
+    void shouldExecuteUnBoundedMethodReference(String name, String expectedResult) {
+        MethodReferenceUnbounded m = new MethodReferenceUnbounded();
+        assertThat(m.apply(name), is(equalTo(expectedResult)));
+    }
+
+}
