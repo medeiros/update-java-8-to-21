87adb98e43151f69c0987d5a515dffbb7ce2a4d5
medeiros <daniel@arneam.com>
2025-02-02 18:16:07 -0300

    Add method reference context

---
+++ src/main/java/com/arneam/java8/lambda/MethodReferenceContext.java
@@ -0,0 +1,36 @@
+package com.arneam.java8.lambda;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class MethodReferenceContext {
+    public int countIntegers() {
+        // compiler infers that it must send zero integers based on the
+        // functional interface type (Supplier takes zero params)
+        Supplier<Integer> supplier = this::howMany;
+        return supplier.get();
+    }
+
+    public int countIntegers(int oneInteger) {
+        // compiler infers that it must send one integer to the howMany function
+        // based on the functional interface type (function takes one param)
+        Function<Integer, Integer> func = this::howMany;
+        return func.apply(oneInteger);
+    }
+
+    public int countIntegers(int oneInteger, int otherInteger) {
+        // compiler infers that it must send two integers to the howMany function
+        // based on the functional interface type (BiFunction takes two params)
+        BiFunction<Integer, Integer, Integer> biFunc = this::howMany;
+        return biFunc.apply(oneInteger, otherInteger);
+    }
+
+    private int howMany(Integer... ints) {
+        return ints.length;
+    }
+
+}
+
+
+
---
+++ src/test/java/com/arneam/java8/lambda/MethodReferenceContextTest.java
@@ -0,0 +1,29 @@
+package com.arneam.java8.lambda;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+class MethodReferenceContextTest {
+
+    @Test
+    void shouldGetZeroIntegers() {
+        MethodReferenceContext m = new MethodReferenceContext();
+        assertThat(m.countIntegers(), is(equalTo(0)));
+    }
+
+    @Test
+    void shouldGetOneInteger() {
+        MethodReferenceContext m = new MethodReferenceContext();
+        assertThat(m.countIntegers(12), is(equalTo(1)));
+    }
+
+    @Test
+    void shouldGetTwoIntegers() {
+        MethodReferenceContext m = new MethodReferenceContext();
+        assertThat(m.countIntegers(12, 24), is(equalTo(2)));
+    }
+
+}
