f26a0d9caf2b90c77deaa191eaaa3add91f13863
medeiros <daniel@arneam.com>
2025-02-02 17:46:13 -0300

    Add static method reference

---
+++ src/main/java/com/arneam/java8/lambda/MethodReferenceStatic.java
@@ -0,0 +1,15 @@
+package com.arneam.java8.lambda;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class MethodReferenceStatic {
+    public void sort(List<Integer> numbers) {
+        // Static method references are unbounded
+        // it can also be written as lambda: "c -> Collections.sort(c)"
+        // (but method reference makes it simpler)
+        Consumer<List<Integer>> consumer = Collections::sort;
+        consumer.accept(numbers);
+    }
+}
---
+++ src/test/java/com/arneam/java8/lambda/MethodReferenceStaticTest.java
@@ -0,0 +1,21 @@
+package com.arneam.java8.lambda;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+
+class MethodReferenceStaticTest {
+
+    @Test
+    void shouldSortListItems() {
+        List<Integer> numbers = Arrays.asList(2, 1, 5, 3, 4);
+        MethodReferenceStatic methodReferenceStatic = new MethodReferenceStatic();
+        methodReferenceStatic.sort(numbers);
+        assertThat(numbers, contains(1, 2, 3, 4, 5));
+    }
+
+}
