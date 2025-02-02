a992a9199dc5366675b3450ffca2530c346165b4
medeiros <daniel@arneam.com>
2025-02-02 18:16:38 -0300

    Remove unused imports

--- src/test/java/com/arneam/java8/lambda/MethodReferenceConstructorTest.java
+++ src/test/java/com/arneam/java8/lambda/MethodReferenceConstructorTest.java
@@ -2,9 +2,7 @@ package com.arneam.java8.lambda;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
