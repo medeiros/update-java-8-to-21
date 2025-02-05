55cff22f54584456d47e72838f6791ec899b9663
Daniel Assis <daniel@arneam.com>
2025-02-05 07:08:47 -0300

    Ad terminal operations for finds and forEach

--- src/main/java/com/arneam/java8/streams/TerminalOperation.java
+++ src/main/java/com/arneam/java8/streams/TerminalOperation.java
@@ -26,4 +26,43 @@ public class TerminalOperation {
         return Comparator.comparingInt(s -> s);
     }
 
+    public int findAny(Integer... values) {
+        // findAny() is a terminal operation but with no reduction,
+        // so once the item is found, the return happens without
+        // all records are being processed.
+        // Reductions reduce the entire stream into one value.
+        Optional<Integer> anyValue = Stream.of(values).findAny();
+        return anyValue.orElse(0);
+    }
+
+    public int findFirst(Integer... values) {
+        // findAny() is a terminal operation but with no reduction,
+        // so once the item is found, the return happens without
+        // all records are being processed.
+        // Reductions reduce the entire stream into one value.
+        Optional<Integer> anyValue = Stream.of(values).findFirst();
+        return anyValue.orElse(0);
+    }
+
+    public boolean findAnyMatch(Integer... values) {
+        // applies predicate
+        // if one matches, returns true
+        return Stream.of(values).anyMatch(s -> s > 2);
+    }
+
+    public boolean findAllMatch(Integer... values) {
+        // applies predicate
+        // if all matches, returns true
+        return Stream.of(values).allMatch(s -> s > 2);
+    }
+
+    public boolean findNoneMatch(Integer... values) {
+        // applies predicate
+        // if none matches, returns true
+        return Stream.of(values).noneMatch(s -> s > 2);
+    }
+
+    public void print (String... values) {
+        Stream.of(values).forEach(System.out::println);
+    }
 }
--- src/test/java/com/arneam/java8/streams/TerminalOperationTest.java
+++ src/test/java/com/arneam/java8/streams/TerminalOperationTest.java
@@ -1,6 +1,9 @@
 package com.arneam.java8.streams;
 
+import com.github.stefanbirkner.systemlambda.SystemLambda;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,6 +29,57 @@ class TerminalOperationTest {
     void shouldCallReductionOperationMax() {
         TerminalOperation terminalOperation = new TerminalOperation();
         assertThat(terminalOperation.max(1, 2, 3), is(equalTo(3)));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1, 2, 3, 1", "3, 2, 1, 3", "2, 1, 3, 2"})
+    void shouldFindAny(int value1, int value2, int value3, int result) {
+        TerminalOperation terminalOperation = new TerminalOperation();
+        assertThat(terminalOperation.findAny(value1, value2, value3),
+                is(equalTo(result)));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1, 2, 3, 1", "3, 2, 1, 3", "2, 1, 3, 2"})
+    void shouldFindFirst(int value1, int value2, int value3, int result) {
+        TerminalOperation terminalOperation = new TerminalOperation();
+        assertThat(terminalOperation.findFirst(value1, value2, value3),
+                is(equalTo(result)));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1, 2, 3, true", "1, 2, 1, false", "4, 1, 3, true"})
+    void shouldFindAnyMatchHigherThan2(int value1, int value2, int value3,
+                                       boolean result) {
+        TerminalOperation terminalOperation = new TerminalOperation();
+        assertThat(terminalOperation.findAnyMatch(value1, value2, value3),
+                is(equalTo(result)));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1, 2, 3, false", "1, 2, 1, false", "3, 4, 5, true"})
+    void shouldFindAllMatchHigherThan2(int value1, int value2, int value3,
+                                       boolean result) {
+        TerminalOperation terminalOperation = new TerminalOperation();
+        assertThat(terminalOperation.findAllMatch(value1, value2, value3),
+                is(equalTo(result)));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1, 2, 3, false", "1, 1, -1, true", "3, 4, 5, false"})
+    void shouldFindNoneMatchHigherThan2(int value1, int value2, int value3,
+                                       boolean result) {
+        TerminalOperation terminalOperation = new TerminalOperation();
+        assertThat(terminalOperation.findNoneMatch(value1, value2, value3),
+                is(equalTo(result)));
+    }
+
+    @Test
+    void shouldPrintData() throws Exception {
+        TerminalOperation terminalOperation = new TerminalOperation();
+        String result = SystemLambda.tapSystemOutNormalized(() ->
+                terminalOperation.print("john"));
+        assertThat(result, is(equalTo("john\n")));
     }
 
 }
